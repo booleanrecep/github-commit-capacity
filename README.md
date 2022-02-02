5bd6e72410db19aa12efa04bb316507d3440256d
Recep Öztürk <recep.fed@gmail.com>
2022-02-03 00:00:18 +0300

    commit

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Test how many commit can be commited in a day to GitHub.
 2
 1
 2
+1
--- commit.sh
+++ commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for i in {1..2}
+for i in {1..100}
 do
    echo $i >> README.md
    git add . 
    git commit -m 'commit'
-   sleep 2s
+   sleep 1s
    git push
 done
