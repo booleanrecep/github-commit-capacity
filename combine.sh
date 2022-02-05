c196f854532a2891e76683f417436ef19c833908
Recep Öztürk <recep.fed@gmail.com>
2022-02-06 01:17:23 +0300

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -2,18 +2,18 @@
 
 git pull
 
-for i in {1..800000}
+for i in {1..1000000}
 do
 #    echo $i >> can/$i.txt
    echo $i >> garbage.txt
    git add . 
-   git commit -m 'github ist bombastisch'
+   git commit --allow-empty -m 'github ist bombastisch'
 
-   if [[ $i%300 -eq 0 ]]
-   then 
-      git push
-   else
-      echo "keep commiting"   
-   fi
+#    if [[ $i%300 -eq 0 ]]
+#    then 
+#       git push
+#    else
+#       echo "keep commiting"   
+#    fi
   
 done
