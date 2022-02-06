de05d9b2a1c99a1cb4153c5d6587a19347e8cf8a
Recep Öztürk <recep.fed@gmail.com>
2022-02-06 20:57:27 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -5,11 +5,11 @@
 source .env
 git pull
 
-for i in $( eval echo {1..$commitCount} )
+for i in $( eval echo {1..$COMMIT_COUNT} )
 do
   git commit --allow-empty -m 'go + git + github = 💥'
 
- if [[ $i%$pushThreshold -eq 0 ]]
+ if [[ $i%$PUSH_THRESHOLD -eq 0 ]]
   then 
     git push 
     echo '🛬 pushed successfully'
