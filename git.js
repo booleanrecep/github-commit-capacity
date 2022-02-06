7013dd9204731b3200fe3037f9edcc0f4303d926
Recep Öztürk <recep.fed@gmail.com>
2022-02-06 19:33:09 +0300

    Update git.js

--- git.js
+++ git.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
-const commitCount = 1000001;
-const pushThreshold = 10000;
+const commitCount = 100001;
+const pushThreshold = 1000;
 
 const gitPull = () => {
   return exec("git pull", (err, stdout, stderr) => {
